314778222810d2fdd0c213dec9fc63fcfe1db59c
Christian Dersch <lupinix@mailbox.org>
2017-07-07 16:36:54 +0200

    implemented string reformats

--- create_kstars_ngcic.py
+++ create_kstars_ngcic.py
@@ -23,4 +23,65 @@
 # SOFTWARE.
 
 from astropy.io import ascii
+import numpy as np
+
+
+def getnum(ngcstr):
+    # Function to extract the integer number of an object,
+    # returns 0 for non-main objects to avoid duplicates
+    if ngcstr[0] == "N":
+        if len(ngcstr) == 7:
+            return ngcstr[3:7]
+        else:
+            return "0"
+    elif ngcstr[0] == "I":
+        if len(ngcstr) == 6:
+            return ngcstr[2:6]
+        else:
+            return "0"
+    else:
+        return "0"
+
+def readable_names(ngcstr):
+    # Generate better readable names, e.g. NGC 224 instead of NGC0224
+    if ngcstr[0] == "N":
+        readable_name = "N " + ngcstr[3:].lstrip("0")
+    elif ngcstr[0] == "I":
+        readable_name = "I " + ngcstr[2:].lstrip("0")
+    else:
+        # Change nothing if don't know what to do
+        readable_name = ngcstr
+    return readable_name
+
+def reformat_ra(ra):
+    if ra[0] == "0" or ra[0] == "1" or ra[0] == "2": 
+        ra_string = ra.replace(":","")
+        return ra_string[:-1] 
+    else:
+        return 8*" "
+
+def reformat_dec(dec):
+    # Omit rows with empty coordinates (nonex objects)
+    if dec[0] == "-" or dec[0] == "+":
+        dec_string = dec.replace(":","")
+        if len(dec_string) == 7:
+            return dec_string
+        elif len(dec_string) == 9:
+            return dec_string[:dec_string.index(".")]
+        else:
+            return 7*" "
+    else:
+        return 7*" "
+
+# Vectorize functions for string reformat operations to run them on whole table
+getnum_vect = np.vectorize(getnum)
+readable_names_vect = np.vectorize(readable_names)
+reformat_ra_vect = np.vectorize(reformat_ra)
+reformat_dec_vect = np.vectorize(reformat_dec)
+
+def create_kstars_table_line(name, ra, dec, bmag, classification, smin, smax, pa, pgc, other, messier, longname):
+    pass
+
+ngc_full = ascii.read("OpenNGC/NGC.csv", delimiter=";")
+ngc = ngc_full[getnum_vect(ngc_full["Name"])!="0"]
 
